a8066fdb7b36d9fdeb26b4c2c735a64261b95ce8
ProgMahabub21 <85748757+ProgMahabub21@users.noreply.github.com>
2022-11-12 00:13:42 +0600

    Add files via upload

---
+++ SuperShop.Entity33/Product.Entity.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperShop.Entity
+{
+  public  class Product
+    {
+        public int productID { get; set; }
+        public string productName { get; set; }
+        public int productCategoryID { get; set; }
+        public double unitPrice { get; set; }
+
+        public int stock { get; set; }
+        public int categoryID { get; set; }
+        public double purchasePrice { get; set; }
+
+        public DateTime expireDate { get; set; }
+
+        public ProductUnit ProductUnit { get; set; }
+
+        public ProductCategory productCategory { get; set; }
+    }
+
+    public class ProductCategory
+    {
+        public string productCategoryName { get; set; }
+        public int productCategoryID { get; set; }
+
+        public ICollection<Product> users { get; set; }
+
+        public override string ToString()
+        {
+            return this.productCategoryName;
+        }
+    }
+
+
+    public class ProductUnit
+    {
+        public string ProductUnitName { get; set; }
+        public int ProductUnitID { get; set; }
+
+        public ICollection<Product> users { get; set; }
+
+        public override string ToString()
+        {
+            return this.ProductUnitName;
+        }
+    }
+
+
+    public class InvoiceProduct : Product
+    {
+     public int qty { get; set; }
+
+
+    }
+}
---
+++ SuperShop.Entity33/Stat.Entity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperShop.Entity
+{
+    public class SalesData
+    {
+        public int NoOfSales{ get; set; }
+        public string SalesmanUsername { get; set; }
+    }
+    public class SealsByDate
+    {
+        public DateTime PurchaseDate { get; set; }
+        public double TotalSales { get; set; }
+    }
+    public class TotalSalesAmount
+    {
+        public double TotalSales { get; set; }
+    }
+}
